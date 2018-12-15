3af96ade673bdadcc6c657dc659f9a7cffec30f9
Mike Galarneau <mbgalarneau@gmail.com>
2018-12-14 19:03:58 -0500

    changed port to test Heroku deployment

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3030;              // localhost
-// const PORT = process.env.PORT; // heroku
+// var PORT = 3030;              // localhost
+const PORT = process.env.PORT; // heroku
 
 // Sets up the Express app to handle data parsing
 // app.use(express.urlencoded({
