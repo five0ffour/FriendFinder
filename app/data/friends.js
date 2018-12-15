0b502bd8136984e7e22306e5b2268e99bb74c3cf
Mike Galarneau <mbgalarneau@gmail.com>
2018-12-14 19:21:58 -0500

    added more survey characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,72 @@ var friends = [
         4,
         1
       ]
+  },
+  {
+    "name":"Wilma Flintstone",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/9/97/Wilma_Flintstone.png",
+    "scores":[
+        4,
+        3,
+        2,
+        1,
+        2,
+        3,
+        4,
+        5,
+        4,
+        3
+      ]
+  },
+  {
+    "name":"Jessica Rabbit",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/8/8d/Jessica_Rabbit.png",
+    "scores":[
+        2,
+        4,
+        1,
+        1,
+        5,
+        2,
+        4,
+        2,
+        1,
+        5
+      ]
+  },
+  {
+    "name":"Aladdin",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/b/be/Aladdin_Disney_pose.png",
+    "scores":[
+        5,
+        2,
+        2,
+        2,
+        1,
+        1,
+        1,
+        3,
+        3,
+        3
+      ]
+  },
+  {
+    "name":"George Jetson",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/6/67/George_Jetson.png",
+    "scores":[
+        2,
+        4,
+        2,
+        3,
+        4,
+        3,
+        4,
+        5,
+        4,
+        3
+      ]
   }
 ];
+
 
 module.exports = friends;
